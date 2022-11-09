fac3682e407e5bb0b592711dd601ed524280c955
Tanushree Dhongale <tanushreedhongale@gmail.com>
2022-11-09 19:54:07 +0530

    added students

--- server/data/studentData.js
+++ server/data/studentData.js
@@ -80,6 +80,258 @@ const studentData = [
             },
         ],
     },
+    {
+        name: "Tanushree",
+        email: "tanushree@ajsbfnd",
+        password: "tanushree",
+        rollno: 304,
+        prn: "19340546t04",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Nikhil",
+        email: "nikhil@ajsbfnd",
+        password: "nikhil",
+        rollno: 354,
+        prn: "19340546t54",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: false,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Tejas",
+        email: "tejas@ajsbfnd",
+        password: "tejas",
+        rollno: 339,
+        prn: "19340546t39",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Rachel",
+        email: "rachel@ajsbfnd",
+        password: "ross",
+        rollno: 364,
+        prn: "19340546t64",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Harry",
+        email: "harry@ajsbfnd",
+        password: "harry",
+        rollno: 343,
+        prn: "19340546t43",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Jesica",
+        email: "jesica@ajsbfnd",
+        password: "jesica",
+        rollno: 325,
+        prn: "19340546t25",
+        semisters: [
+            {
+                semisterNumber: 1,
+                totalAttendance: 95,
+                subject: [
+                    {
+                        subjectId: "BTCOE",
+                        subjectName: "Cloud",
+                        total: "56",
+                        dates: [
+                            {
+                                date: 1987 - 9 - 28,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 27,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 26,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 25,
+                                ispresent: true,
+                            },
+                            {
+                                date: 1987 - 9 - 24,
+                                ispresent: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    }
 ];
 
 export default studentData;
