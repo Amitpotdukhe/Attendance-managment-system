3994235acf7998322a39aad217997e59b9040943
Amitpotdukhe <69644033+Amitpotdukhe@users.noreply.github.com>
2022-12-18 15:28:23 +0530

    dashboard

---
+++ frontend/src/components/table/Table.jsx
@@ -0,0 +1,95 @@
+import "./table.scss";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const List = () => {
+  const rows = [
+    {
+      id: 1143155,
+      product: "Amit Potdukhe",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: "78%",
+      status: "Present",
+    },
+    {
+      id: 2235235,
+      product: "Nikhil Tidke",
+      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Michael Doe",
+      date: "1 March",
+      amount: "90%",
+      status: "Absent",
+    },
+    {
+      id: 2342353,
+      product: "Tejes Borade",
+      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: "35%",
+      status: "Absent",
+    },
+    {
+      id: 2357741,
+      product: "Tanushree Dongale",
+      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Jane Smith",
+      date: "1 March",
+      amount: "92%",
+      status: "Present",
+    },
+    {
+      id: 2342355,
+      product: "Prajwal Bhasme",
+      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Harold Carol",
+      date: "1 March",
+      amount: "20%",
+      status: "Absent",
+    },
+  ];
+  return (
+    <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="tableCell">Prn No.</TableCell>
+            <TableCell className="tableCell">Subject</TableCell>
+            <TableCell className="tableCell">Full Name</TableCell>
+            <TableCell className="tableCell">Date</TableCell>
+            <TableCell className="tableCell">Total Attendance</TableCell>
+            <TableCell className="tableCell">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className="tableCell">{row.id}</TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
+              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.date}</TableCell>
+              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default List;
--- server/routes/facultyRoute.js
+++ server/routes/facultyRoute.js
@@ -7,48 +7,6 @@ import { LocalStorage } from "node-localstorage";
 var localStorage = new LocalStorage("./users");
 const router = express.Router();
 
-// router.get("/", protect, async (req, res, next) => {
-//     const faculties = await Faculty.find();
-//     if (!faculties) {
-//         next(new Error("Cannot fetch faculties"));
-//     } else {
-//         res.status(200).json({ faculties });
-//     }
-// });
-
-// router.route("/signup").post(async (req, res, next) => {
-//     const { facultyName, role, password, email } = req.body;
-
-//     const facultyUser = await Faculty.findOne({ email });
-
-//     if (facultyUser) {
-//         next(new Error("Faculty already exist"));
-//     } else {
-//         const newFaculty = await Faculty.create({
-//             facultyName,
-//             role,
-//             password,
-//             email,
-//         });
-
-//         if (newFaculty) {
-//             res.status(201).json({
-//                 _id: newFaculty._id,
-//                 facultyName: newFaculty.facultyName,
-//                 role: newFaculty.role,
-//                 email: newFaculty.email,
-//                 token: null,
-//             });
-//         } else {
-//             next(
-//                 new Error(
-//                     "Invalid Faculty details, Please enter correct details"
-//                 )
-//             );
-//         }
-//     }
-// });
-
 router.post("/login", async (req, res, next) => {
     const data = req.body;
     console.log(data);
