495796b52f11e591a5bd92fdeb959a8a7aa80b3a
Nikhil Tidke <nikhil.st8@gmail.com>
2022-12-27 14:19:48 +0530

    FacultyDashbord and StudentDashboard Routes

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import FacultyDashboard from "./layout/FacultyDashboard/FacultyDashboard";
+import StudentDashboard from "./layout/StudentDashboard/StudentDashboard";
 import Login from "./layout/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-            <BrowserRouter>
-                <Routes>
-                      <Route path="/" element={<Login />} />
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                  <Route path="/" element={<Login />} />
+                  <Route path="/faculty/:id" element={<FacultyDashboard/>} />
+                  <Route path="/student/:id" element={<StudentDashboard/>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
